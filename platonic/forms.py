e7b03c9237d51d5808395b59f725608a1df8e38b
Sam Ireland <sam.ireland.uk@gmail.com>
2018-03-24 22:33:08 +0000

    Tidy up forms

--- platonic/forms.py
+++ platonic/forms.py
@@ -1,16 +1,30 @@
 from .fields import Field
 
 class Form:
+    """Represents a form - an object with a list of data entry fields, the
+    ability to produce an HTML representation of itself, and the ability to
+    validate the data that is entered into it.
+
+    When an instance of a form is created, the attributes of the Form class will
+    be checked. If it is a field, four things will happen - (1) a copy of the
+    field object will be made, (2) the form instance's field will be set to the
+    copy rather than the original that lives in the class, (3) the field will
+    be appended to the form's ``field`` attribute, (4) the new field's name will
+    be set to whatever the class variable name was.
+
+    Of course the base Form class has no such class attributes, but subclasses
+    the user creates will.
+
+    Forms are iterables of their fields."""
 
     def __new__(cls):
-        form = super(Form, cls).__new__(cls)
-        form._fields = []
+        form, form._fields = super(Form, cls).__new__(cls), []
         for attribute_name, attribute_obj in cls.__dict__.items():
             if isinstance(attribute_obj, Field):
                 new_field = attribute_obj.copy()
                 form.__dict__[attribute_name] = new_field
                 form._fields.append(new_field)
-                new_field._name = attribute_name
+                new_field.name = attribute_name
         return form
 
 
@@ -28,4 +42,8 @@ class Form:
 
     @property
     def fields(self):
+        """The fields that the form has, in the order that they were set.
+
+        :rtype" ``tuple``"""
+
         return tuple(self._fields)
--- tests/unit/test_forms.py
+++ tests/unit/test_forms.py
@@ -9,11 +9,11 @@ class FormTest(TestCase):
     def setUp(self):
         class FormSub(Form): pass
         self.Form = FormSub
-        self.field1 = Mock(Field)
-        self.field2 = Mock(Field)
-        self.field1.copy.return_value = self.field1
-        self.field2.copy.return_value = self.field2
-        self.Form.f1, self.Form.f2, self.Form.x = self.field1, self.field2, "a"
+        self.fieldA, self.fieldB = Mock(Field), Mock(Field)
+        self.field1, self.field2 = Mock(Field), Mock(Field)
+        self.Form.f1, self.Form.f2, self.Form.x = self.fieldA, self.fieldB, "a"
+        self.fieldA.copy.return_value = self.field1
+        self.fieldB.copy.return_value = self.field2
 
 
 
@@ -21,13 +21,13 @@ class FormCreationTests(FormTest):
 
     def test_can_create_form_with_fields(self):
         form = self.Form()
-        self.assertEqual(form._fields, [self.field1, self.field2])
+        self.fieldA.copy.assert_called_with()
+        self.fieldB.copy.assert_called_with()
         self.assertIs(form.f1, self.field1)
         self.assertIs(form.f2, self.field2)
-        self.assertEqual(form.f1._name, "f1")
-        self.assertEqual(form.f2._name, "f2")
-        self.field1.copy.assert_called_with()
-        self.field2.copy.assert_called_with()
+        self.assertEqual(form._fields, [self.field1, self.field2])
+        self.assertEqual(form.f1.name, "f1")
+        self.assertEqual(form.f2.name, "f2")
 
 
 
